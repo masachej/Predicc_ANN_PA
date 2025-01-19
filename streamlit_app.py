79203a89d8c43995332d7578f5d9274ccb352b6c
Jose Luis Masache Valdiviezo <166836763+masachej@users.noreply.github.com>
2025-01-19 09:32:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,19 +22,21 @@ st.title("Predicción de Producción de Azúcar")
 
 # Texto explicativo sobre la utilidad del aplicativo
 st.write("""
-Este aplicativo permite predecir la producción de azúcar a partir de tres variables clave: TCM, Rendimiento y Toneladas de Jugo.
-La herramienta es útil para los profesionales en la industria azucarera, facilitando la toma de decisiones informadas basadas en datos.
-Ingrese los valores en los campos a continuación para obtener una estimación de la producción.
+Este aplicativo permite predecir la producción de azúcar a partir de tres variables clave: Toneladas Caña Molida(TCM), Rendimiento y Toneladas de Jugo.
+La herramienta es útil para los profesionales en la industria azucarera, facilitando la toma de decisiones informadas basadas en datos.""")
+
+st.write("""
+La predicción se realiza mediante un algoritmo de machine learning, utilizando una Red Neuronal Artificial (ANN) entrenada con datos históricos diarios de producción azucarera del Ingenio Azucarero Monterrey C.A.
 """)
 
 st.write("""
-La predicción se realiza mediante un algoritmo de machine learning, utilizando una Red Neuronal Artificial (ANN) entrenada con datos históricos diarios de producción azucarera del Ingenio Azucarero Monterrey C.A, una industria Lojana.
+Ingrese los valores en los campos a continuación para obtener una estimación de la producción.
 """)
 
 # Entrada de datos
-tcm = st.number_input("Ingrese el valor de TCM", min_value=0.0, value=0.0, step=0.01)
-rendimiento = st.number_input("Ingrese el valor de Rendimiento", min_value=0.0, value=0.0, step=0.01)
-toneladas_jugo = st.number_input("Ingrese el valor de Toneladas de Jugo", min_value=0.0, value=0.0, step=0.01)
+tcm = st.number_input("Ingrese el valor de Toneladas Caña Molida ( ton)", min_value=0.0, value=0.0, step=0.01)
+rendimiento = st.number_input("Ingrese el valor de Rendimiento (kg/ TCM)", min_value=0.0, value=0.0, step=0.01)
+toneladas_jugo = st.number_input("Ingrese el valor de Toneladas de Jugo (ton)", min_value=0.0, value=0.0, step=0.01)
 
 # Botón para hacer la predicción
 if st.button("Realizar Predicción"):
