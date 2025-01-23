7bfcabaa16e35d2b0f6cc5a2e3402bfb999c1a1a
Jose Luis Masache Valdiviezo <166836763+masachej@users.noreply.github.com>
2025-01-22 22:59:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ def make_prediction(tcm, rendimiento, toneladas_jugo):
 st.title("MONTERREY AZUCARERA LOJANA")
 
 # Cargar el logo
-logo_path = "logo.png"  # Cambia a la ruta correcta si es necesario
+logo_path = "logom.png"  # Cambia a la ruta correcta si es necesario
 if os.path.exists(logo_path):
     # Usamos HTML para centrar el logo
     st.markdown(
